0337d79c7936a2848805a36a2bda5b615aa82e17
Akshay Gaikwad <akgaikwad001@gmail.com>
2020-01-28 20:24:18 +0530

    Add tests login template
    This tests login.html template for root url.
    
    Signed-off-by: Akshay Gaikwad <akgaikwad001@gmail.com>

--- logtoday/tests.py
+++ logtoday/tests.py
@@ -7,3 +7,7 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_index_page_view(self):
         found = resolve("/")
         self.assertEqual(found.view_name, "index")
+
+    def test_uses_login_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'login.html')
