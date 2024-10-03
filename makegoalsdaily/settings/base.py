09fe9652f9fa2030a502e1f4803f1c28fad00d59
Sundeep Anand <sundeep.sdp5@gmail.com>
2024-10-03 13:23:18 +0930

    Dockerfile cleanup and db path env

--- makegoalsdaily/settings/base.py
+++ makegoalsdaily/settings/base.py
@@ -75,13 +75,14 @@ WSGI_APPLICATION = 'makegoalsdaily.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DATABASE_PATH = os.environ.get("DB_PATH", BASE_DIR)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DATABASE_PATH, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
